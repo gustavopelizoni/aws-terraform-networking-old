4212b5264d4937ecda30137c034a7d7d874f796a
gustavo.pelizoni <aws@bild.com.br>
2021-08-24 12:14:22 -0300

    update remote

---
+++ organization-bi/main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+
+#provider "aws" {
+ # region = terraform.workspace == "prod" ? "us-east-1" : "us-east-1"
+  # profile = var.aws_profile
+#}
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "Almee"
+
+    workspaces {
+      name = "aws-terraform-networking-bi"
+    }
+  }
+}
