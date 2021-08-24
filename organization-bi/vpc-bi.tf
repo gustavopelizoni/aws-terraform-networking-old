c20a587ace895912e3507ac86dbe4e2eb5ae9d10
gustavo.pelizoni <aws@bild.com.br>
2021-08-24 17:18:45 -0300

    update sg 2

--- organization-bi/vpc-bi.tf
+++ organization-bi/vpc-bi.tf
@@ -104,8 +104,8 @@ resource "aws_security_group" "sg-vpc-biviholding" {
       from_port        = 443
       to_port          = 443
       protocol         = "tcp"
-      cidr_blocks      = [aws_vpc.main.cidr_block]
-      ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+      cidr_block      = [aws_vpc.main.cidr_block]
+      #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
     }
   ]
 
@@ -114,8 +114,8 @@ resource "aws_security_group" "sg-vpc-biviholding" {
       from_port        = 0
       to_port          = 0
       protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
+      cidr_block      = ["0.0.0.0/0"]
+      #ipv6_cidr_blocks = ["::/0"]
     }
   ]
 
