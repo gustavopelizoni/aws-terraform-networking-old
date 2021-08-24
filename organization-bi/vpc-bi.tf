dc81c089470bd34806405ad86f1732e47fa75ae0
gustavo.pelizoni <aws@bild.com.br>
2021-08-24 19:50:38 -0300

    update tf

--- organization-bi/vpc-bi.tf
+++ organization-bi/vpc-bi.tf
@@ -38,7 +38,80 @@ resource "aws_subnet" "PROD-PUB-D" {
 }
 
 #####Subnet Public PRD
+#SUBNET PROD-PUB-A
+resource "aws_subnet" "PROD-PUB-A" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.PROD-PUB-A
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name     = "PROD-PUB-A"
+    NameArea = "Infra"
+  }
+}
+
+#SUBNET PROD-PUB-B
+resource "aws_subnet" "PROD-PUB-B" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.PROD-PUB-B
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name     = "PROD-PUB-B"
+    NameArea = "Infra"
+  }
+}
+
+#SUBNET PROD-PUB-C
+resource "aws_subnet" "PROD-PUB-C" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.PROD-PUB-C
+  availability_zone = "us-east-1c"
+
+  tags = {
+    Name     = "PROD-PUB-C"
+    NameArea = "Infra"
+  }
+}
+
+#SUBNET PROD-PUB-E
+resource "aws_subnet" "PROD-PUB-E" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.PROD-PUB-E
+  availability_zone = "us-east-1e"
+
+  tags = {
+    Name     = "PROD-PUB-E"
+    NameArea = "Infra"
+  }
+}
+
+
 #####Subnet Public DEV
+#SUBNET DEV-PUB-D
+resource "aws_subnet" "DEV-PUB-D" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.DEV-PUB-D
+  availability_zone = "us-east-1d"
+
+  tags = {
+    Name     = "DEV-PUB-D"
+    NameArea = "Infra"
+  }
+}
+
+#SUBNET DEV-PUB-E
+resource "aws_subnet" "DEV-PUB-E" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.DEV-PUB-E
+  availability_zone = "us-east-1e"
+
+  tags = {
+    Name     = "DEV-PUB-"
+    NameArea = "Infra"
+  }
+}
+
 #####Subnet Public HML
 #SUBNET HML-PUB-D
 resource "aws_subnet" "HML-PUB-D" {
@@ -52,11 +125,25 @@ resource "aws_subnet" "HML-PUB-D" {
   }
 }
 
+resource "aws_subnet" "HML-PUB-E" {
+  vpc_id            = aws_vpc.biviholding.id
+  cidr_block        = var.HML-PUB-E
+  availability_zone = "us-east-1e"
+
+  tags = {
+    Name     = "HML-PUB-E"
+    NameArea = "Infra"
+  }
+}
+
 #####Subnet Private PROD
 #SUBNET PROD-PRV-A
 resource "aws_subnet" "PROD-PRV-A" {
   vpc_id     = aws_vpc.biviholding.id
   cidr_block = var.PROD-PRV-A
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+  
 
   tags = {
     Name     = "PROD-PRV-A"
@@ -68,6 +155,8 @@ resource "aws_subnet" "PROD-PRV-A" {
 resource "aws_subnet" "PROD-PRV-B" {
   vpc_id     = aws_vpc.biviholding.id
   cidr_block = var.PROD-PRV-B
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
 
   tags = {
     Name     = "PROD-PRV-B"
@@ -75,9 +164,63 @@ resource "aws_subnet" "PROD-PRV-B" {
   }
 }
 
+#SUBNET PROD-PRV-C
+resource "aws_subnet" "PROD-PRV-C" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.PROD-PRV-C
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name     = "PROD-PRV-C"
+    NameArea = "Infra"
+  }
+}
+
 #####Subnet Private DEV
 
 #####Subnet Private HML
+#SUBNET HML-PRIV-A
+resource "aws_subnet" "HML-PRIV-A" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.HML-PRIV-A
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name     = "HML-PRIV-A"
+    NameArea = "Infra"
+  }
+}
+
+#####Subnet Private HML
+#SUBNET HML-PRIV-B
+resource "aws_subnet" "HML-PRIV-B" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.HML-PRIV-B
+  availability_zone = "us-east-1b"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name     = "HML-PRIV-B"
+    NameArea = "Infra"
+  }
+}
+
+#####Subnet Private HML
+#SUBNET HML-PRIV-C
+resource "aws_subnet" "HML-PRIV-C" {
+  vpc_id     = aws_vpc.biviholding.id
+  cidr_block = var.HML-PRIV-C
+  availability_zone = "us-east-1c"
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name     = "HML-PRIV-C"
+    NameArea = "Infra"
+  }
+}
+
 
 #SUBNET HML-PRIV-C
 resource "aws_subnet" "HML-PRIV-C" {
