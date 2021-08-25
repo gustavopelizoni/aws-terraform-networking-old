7de8e4e4b49bee455ce1d3cb4469aa373b136d04
gustavo.pelizoni <aws@bild.com.br>
2021-08-25 00:02:12 -0300

    update nat

--- organization-bi/vpc-bi.tf
+++ organization-bi/vpc-bi.tf
@@ -296,7 +296,7 @@ resource "aws_route_table" "rt-biviholding-private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat-gw-biviholding
+    nat_gateway_id = aws_nat_gateway.nat-gw-biviholding
   }
 
   tags = {
