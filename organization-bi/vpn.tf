3bd728be0054aa60b7a3b519cb5252f70f65a45d
gustavo.pelizoni <aws@bild.com.br>
2021-08-26 18:16:24 -0300

    update vpn

--- organization-bi/vpn.tf
+++ organization-bi/vpn.tf
@@ -1,7 +1,7 @@
 #customer gateway
 resource "aws_customer_gateway" "Oracle" {
   bgp_asn    = 65000
-  ip_address = "168.138.243.33"
+  ip_address = "168.138.243.33" #ip gateway oracle cloud
   type       = "ipsec.1"
 
   tags = {
@@ -16,4 +16,20 @@ resource "aws_vpn_gateway" "Oracle" {
   tags = {
     Name = "Oracle"
   }
+}
+
+## site to site Oracle
+resource "aws_vpc" "Oracle" {
+  cidr_block = "10.251.0.0/17"
+}
+
+resource "aws_vpn_connection" "Oracle" {
+  vpn_gateway_id      = aws_vpn_gateway.Oracle.id
+  customer_gateway_id = aws_customer_gateway.Oracle.id
+  type                = "ipsec.1"
+  static_routes_only  = true
+  #tunnel1_inside_cidr = "0.0.0.0/0"
+  tunnel1_preshared_key = "F23m732vhFwpWVgEt5gOh4JzN4jPvHiZfh1oE7Gzo2PeBPu29fljvVQZVqiqlZ0o"
+  #vpc_subnet_route_table_count = 1
+  #vpc_subnet_route_table_id   = "rtb-0e0d348cfee9eea8c"
 }
