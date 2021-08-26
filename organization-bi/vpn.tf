4ba2ee8ba2e7402bd5571943d3cc80150830cc32
gustavo.pelizoni <aws@bild.com.br>
2021-08-26 17:40:27 -0300

    update private gateway

--- organization-bi/vpn.tf
+++ organization-bi/vpn.tf
@@ -11,7 +11,7 @@ resource "aws_customer_gateway" "Oracle" {
 
 #Virtual private gateway
 resource "aws_vpn_gateway" "Oracle" {
-  vpc_id = aws_vpc.biviholding
+  vpc_id = aws_vpc.biviholding.id
 
   tags = {
     Name = "Oracle"
