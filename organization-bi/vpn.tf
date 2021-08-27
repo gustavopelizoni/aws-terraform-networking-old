cdffdabcb1c0573fbe14e6d6f0f6be535a6cdfb9
gustavo.pelizoni <aws@bild.com.br>
2021-08-27 09:20:49 -0300

    update vpn

--- organization-bi/vpn.tf
+++ organization-bi/vpn.tf
@@ -1,7 +1,7 @@
 #customer gateway
 resource "aws_customer_gateway" "Oracle" {
   bgp_asn    = 65000
-  ip_address = "168.138.243.33" #ip gateway oracle cloud
+  ip_address = "168.138.240.215" #ip gateway oracle cloud
   type       = "ipsec.1"
 
   tags = {
@@ -25,17 +25,17 @@ resource "aws_vpn_connection" "Oracle" {
   type                = "ipsec.1"
   static_routes_only  = true
   #tunnel1_inside_cidr = "0.0.0.0/0"
-  tunnel1_preshared_key = "F23m732vhFwpWVgEt5gOh4JzN4jPvHiZfh1oE7Gzo2PeBPu29fljvVQZVqiqlZ0o"
+  tunnel1_preshared_key = "7wrtqrh5HBZHD0LyXaPamWzCQt2q74lqH8UCEpz8qcIFWcv7af4DLDE8opAy29xQ"
   #vpc_subnet_route_table_count = 1
   #vpc_subnet_route_table_id   = "rtb-0e0d348cfee9eea8c"
   #destination_cidr_block   = "172.31.0.0/24"
+  ##static route site to site
+  resource "aws_vpn_connection_route" "Oracle" {
+    destination_cidr_block = "172.31.0.0/24"
+    vpn_connection_id      = aws_vpn_connection.Oracle.id
+  }
+
   tags = {
     "Name" = "Oracle"
   }
-}
-
-
-resource "aws_vpn_connection_route" "Oracle" {
-  destination_cidr_block = "172.31.0.0/24"
-  vpn_connection_id      = aws_vpn_connection.Oracle.id
 }
