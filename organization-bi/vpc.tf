e3c3a613996df92e011ae7cc7093e2ab5a40cc4e
gustavo.pelizoni <aws@bild.com.br>
2021-08-27 14:24:15 -0300

    nat

--- organization-bi/vpc.tf
+++ organization-bi/vpc.tf
@@ -294,7 +294,7 @@ resource "aws_route_table" "rt-biviholding-private" {
 
   route {
     cidr_block = "0.0.0.0/0"
-    gateway_id = aws_nat_gateway.nat-gw-biviholding.id
+    nat_gateway_id = aws_nat_gateway.nat-gw-biviholding.id
   }
 
   tags = {
