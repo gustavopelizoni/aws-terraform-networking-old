410a431710f7264b4fee0175d5a53d514293318d
gustavo.pelizoni <aws@bild.com.br>
2021-08-27 08:51:31 -0300

    update vpc

--- organization-bi/vpn.tf
+++ organization-bi/vpn.tf
@@ -28,7 +28,7 @@ resource "aws_vpn_connection" "Oracle" {
   tunnel1_preshared_key = "F23m732vhFwpWVgEt5gOh4JzN4jPvHiZfh1oE7Gzo2PeBPu29fljvVQZVqiqlZ0o"
   #vpc_subnet_route_table_count = 1
   #vpc_subnet_route_table_id   = "rtb-0e0d348cfee9eea8c"
-  aws_vpn_connection_route = "Oracle"
+  aws_vpn_connection_route = "rtb-0e0d348cfee9eea8c"
   destination_cidr_block   = "172.31.0.0/24"
   tags = {
     "Name" = "Oracle"
