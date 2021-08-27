b792ccc6b4850e1077f185d7fc4e69e6351f0e29
gustavo.pelizoni <aws@bild.com.br>
2021-08-27 15:22:42 -0300

    update 168.138.236.98

--- organization-bi/vpn.tf
+++ organization-bi/vpn.tf
@@ -13,7 +13,7 @@ resource "aws_customer_gateway" "Oracle" {
 #customer gateway
 resource "aws_customer_gateway" "OracleCloud" {
   bgp_asn    = 65000
-  ip_address = "35.173.147.143" #ip gateway oracle cloud
+  ip_address = "168.138.236.98" #ip gateway oracle cloud
   type       = "ipsec.1"
 
   tags = {
