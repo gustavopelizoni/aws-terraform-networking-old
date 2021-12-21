ba665f83a862446a6747f9ff9b9c43d7244b4065
gustavo.pelizoni <aws@bild.com.br>
2021-12-21 01:27:43 -0300

    udpate

--- organization-bi/peering.tf
+++ organization-bi/peering.tf
@@ -1,14 +1,43 @@
-resource "aws_vpc_peering_connection" "biviholding" {
-  peer_owner_id = var.peer_owner_id
-  peer_vpc_id   = aws_vpc.biviholding-RAIZ.id
-  vpc_id        = aws_vpc.biviholding.id
+resource "aws_vpc_peering_connection" "owner" {
+  provider = aws.owner
+  vpc_id        = var.owner_vpc_id
+  peer_vpc_id   = data.aws_vpc.accepter.id
+  peer_owner_id = local.accepter_account_id
+  
+  tags = {
+    Name = "peer_to_accepter"
+  }
+
 }
 
-resource "aws_vpc" "biviholding-RAIZ" {
-  cidr_block = "10.250.0.0/17"
+resource "aws_vpc_peering_connection_accepter" "accepter" {
+  provider = aws.accepter
+  vpc_peering_connection_id = aws_vpc_peering_connection_owner.id
+  auto_accept = true
+  tags = {
+    Name = "peert_to_owner"
+  }
+  
+}
 
 
+resource "aws_route" "owner" {
+  provider = aws.owner
+  count = length(data.aws_route_tables.owner.ids)
+  route_table_id = tolist(data.aws_route_tables.owner.ids)[count.index]
+  destination_cidr_block = data.aws_vpc_peering_connection_owner.id
+  
+}
 
+resource "aws_route" "accepter" {
+  provider = aws.accepter
+  count = length(data.aws_route_tables.accepter.ids)
+  route_table_id = tolist(data.aws_route_tables.accepter.ids)[count.index]
+  destination_cidr_block = data.aws_vpc.owner.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.owner.id
+  
+
+  
 }
 
 resource "aws_route" "rt-biviholding-private" {
